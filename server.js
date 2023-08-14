750e80b2ebd8fabfb20cbcb43b655d14959593fd
Duvnjak-Ibrahim <duvnjak.ibrahim2004@gmail.com>
2023-08-14 22:10:12 +0200

    13commit

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const connectDb = require("./config/dbConnection");
 const { constants } = require("./constans");
-const dotenv = require("dotenv").config()
 const cors = require("cors")
+const path = require("path")
+require("dotenv").config()
 
 connectDb();
 const app = express()
@@ -14,13 +15,20 @@ app.use(cors(
 }
 ))
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3001;
 
 app.use(express.json())
 app.use("/api/posts",require("./routes/postRoutes"))
 app.use("/api/users",require("./routes/userRoutes"))
 
-
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static("client/build"))
+    app.get("*",(req,res)=>{
+        res.sendFile(
+            path.resolve(_dirname,"client","build","index.html")
+            )
+    })
+}
 
 app.listen(port, ()=>{
     console.log(`server on ${port}`);
